40531e89ad00d9a911acc85560602d9d429ff3ed
Ása Björk V <sa Bjrk V>
2016-05-16 01:25:12 +0000

    added arguments to CLI

--- y_u_so_stupid_CLI.py
+++ y_u_so_stupid_CLI.py
@@ -3,13 +3,29 @@ import sys
 import json
 import y_u_so_stupid
 import time
+import argparse
 
+'''
+    ARGUMENT PARSING
+'''
+parser = argparse.ArgumentParser(description='Y U so stupid!?')
+parser.add_argument('-q', '--questions', help='number of questions to run', type=int, nargs=1)
+
+args = parser.parse_args()
+
+'''
+    MAIN FUNCTION
+'''
 def playCLI():
     score = 0
+    if args.questions:
+        numberOfQuestions = args.questions[0]
+    else:
+        numberOfQuestions = 10
     
     printAndFlush("~Welcome to 'y u so stupid?'~\nNow, don't be stupid mkey?\n\nAre you ready?\n...\n")
 
-    for i in range(10):
+    for i in range(numberOfQuestions):
         question = json.loads(y_u_so_stupid.getRandomQuestion())
 
         printAndFlush(question['question'])
@@ -31,11 +47,10 @@ def playCLI():
         else:
             printAndFlush("WRONG... y u so stupid?\nAnswer: {0}\n".format(question['answer']))
         
-    printAndFlush("Your score is: {0}/100".format(score))
+    printAndFlush("Your score is: {0}/100".format(int(score/(numberOfQuestions*10)*100)))
 
 def printAndFlush(string):
     print(string)
     sys.stdout.flush()
 
-
-
+playCLI()
